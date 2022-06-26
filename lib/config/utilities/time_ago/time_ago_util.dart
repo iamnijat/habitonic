629de0631f9afd29f2e6c071dd032f6696bbb820
Nijat Namaz <nicat.namazzade@gmail.com>
2022-06-26 12:46:23 +0400

    initialized ci integration

--- lib/config/utilities/time_ago/time_ago_util.dart
+++ lib/config/utilities/time_ago/time_ago_util.dart
@@ -1,26 +1,24 @@
 import 'package:timeago/timeago.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'custom_en_messages.dart';
-import 'custom_fr_messages.dart';
-import 'custom_tr_messages.dart';
+import 'custom_time_ago_localization_messages.dart';
 
 class TimeAgoUtil {
   const TimeAgoUtil._();
 
   static String getTimeDifference(
       DateTime _date, AppLocalizations _localization) {
-    final _localeName = _localization.localeName;
     _setCustomLocaleMessages(_localization);
 
-    return _formattedDate(_date, _localeName);
+    return _formattedDate(_date, _localeName(_localization));
   }
 
   static String _formattedDate(DateTime _date, String _locale) =>
       format(_date, locale: _locale);
 
   static void _setCustomLocaleMessages(AppLocalizations _localization) {
-    setLocaleMessages('fr', CustomFrMessages(_localization));
-    setLocaleMessages('en', CustomEnMessages(_localization));
-    setLocaleMessages('tr', CustomTrMessages(_localization));
+    setLocaleMessages(_localization.localeName,
+        CustomTimeAgoLocalizationMessages(_localization));
   }
+
+  static String _localeName(AppLocalizations _lcl) => _lcl.localeName;
 }
